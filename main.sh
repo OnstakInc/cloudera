f7574dbcae9035b401ac6f6410a3783a3b597ecf
Raheel Anwar <raheelanwar74@gmail.com>
2018-08-29 17:16:41 +0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -8,10 +8,7 @@ yum update -y
 
 yum install oracle-j2sdk1.7 -y
 
-yum install mysql mysql-server mysql-connector-java -y
-
-service mysqld start
-/usr/bin/mysqladmin -u root password 'root'
+yum install mysql mysql-connector-java -y
 
 yum install cloudera-manager-daemons cloudera-manager-server -y
 
